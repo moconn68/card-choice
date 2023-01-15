e3922b6430f472ece833546bac9ee0bab1470462
Matt OConnell <mattgoconn@gmail.com>
2023-01-15 02:13:04 -0800

    Initial commit
    Initial commit for the Card Choice Date Night web app. This commit
    implements the initial base UI elements, including the `Card` component
    with logic and styling to incorporate a flip-over effect on click.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Renders two Card elements", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const cardElements = screen.getAllByLabelText("flip-card");
+  expect(cardElements).toHaveLength(2);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+interface CardProps {
+  iconUri?: string;
+  text: string;
+}
+function Card(props: CardProps): JSX.Element {
+  const { iconUri, text } = props;
+  const [flippedClass, setFlippedClass] = useState<string>("");
+  function handleClick(event: React.MouseEvent) {
+    setFlippedClass((state) => {
+      return state === "" ? "flip-anim" : "";
+    });
+  }
+  return (
+    <div aria-label="flip-card" className="flip-card" onClick={handleClick}>
+      <div className={`flip-card-inner ${flippedClass}`}>
+        <div className="flip-card-front">
+          {iconUri ? (
+            <img
+              src={iconUri}
+              alt="Card Icon"
+              style={{ width: 100, height: 100 }}
+            />
+          ) : (
+            <h1>?</h1>
+          )}
+        </div>
+        <div className="flip-card-back">
+          <h1>{text}</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cards-container">
+        <Card text="First Card" />
+        <Card text="Second Card" />
+      </div>
     </div>
   );
 }
